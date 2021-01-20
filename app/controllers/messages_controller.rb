0347f99c16f8d4ad0b1775593fdea70146090c2a
MaruyakiLog <auklm0015@gmail.com>
2021-01-20 21:41:47 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,17 @@
 class MessagesController < ApplicationController
   def index
-    # binding.pry
-    @room = Room.find(params[:room_id])
     @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    # binding.pry
     if @message.save then
       redirect_to room_messages_path(@room)
     else
-      render :index 
+      render :index
+      @messages = @room.messages.includes(:user)
     end
   end
 
@@ -19,12 +19,4 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
-
-  # private
-  # def room_param
-  #   param.require(:room).permit(:id)
-  # end
-  # def message_param
-  #   param.require(:message).permit()
-  # end
 end
