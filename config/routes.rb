b127592a23e4631792ab41b84da0169d6b24392d
MaruyakiLog <auklm0015@gmail.com>
2021-01-25 01:58:44 +0900

    結合テストの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   # root to: "messages#index"
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-<<<<<<< Updated upstream
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
-=======
-  resources :rooms, only: [:new, :create]
-end
->>>>>>> Stashed changes
